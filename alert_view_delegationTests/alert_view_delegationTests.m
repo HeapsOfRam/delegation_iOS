b058fde99bec55d930b242558479b5282bf702e3
Ryan March <rmarch03@villanova.edu>
2014-11-18 09:11:54 -0500

    Initial Commit

---
+++ alert_view_delegationTests/alert_view_delegationTests.m
@@ -0,0 +1,40 @@
+//
+//  alert_view_delegationTests.m
+//  alert_view_delegationTests
+//
+//  Created by Ryan March on 11/18/14.
+//  Copyright (c) 2014 Ryan March. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface alert_view_delegationTests : XCTestCase
+
+@end
+
+@implementation alert_view_delegationTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
